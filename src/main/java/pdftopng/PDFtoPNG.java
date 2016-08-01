afd00cd331cf0b5aaa0594195f8833d7861f0782
my-tasks <krystsina_kharlamenkova@epam.com>
2016-08-01 21:55:23 +0300

    initial commit

---
+++ src/main/java/pdftopng/PDFtoPNG.java
@@ -0,0 +1,14 @@
+package pdftopng;
+
+import org.im4java.core.ConvertCmd;
+
+public class PDFtoPNG {
+
+	static String PDF_PATH = "test.pdf";
+	static String PNG_PATH = "test.png";
+	
+	public static void main(String[] args) {
+	  ConvertCmd cmd = new  ConvertCmd();
+	  System.out.println("DONE");
+	}
+}
